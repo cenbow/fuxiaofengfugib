1f44c2d81449a9e59ef3abb3b20da71013d3188a
fuxiaofengfu <fuxiaofeng1107@163.com>
2016-12-20 16:35:03 +0800

    修改注释

--- TestWeb/src/biodemo/package-info.java
+++ TestWeb/src/biodemo/package-info.java
@@ -1,14 +1,2 @@
-/**
- * Copyright (c) 2016 CQLIVING, Inc. All rights reserved.
- * This software is the confidential and proprietary information of 
- * CQLIVING, Inc. You shall not disclose such Confidential
- * Information and shall use it only in accordance with the terms of the 
- * license agreement you entered into with CQLIVING.
- */
-/**
- * Title:
- * <p>Description:</p>
- * Copyright (c) CQLIVING 2016
- * @author fuxiaofeng on 2016年11月16日
- */
+
 package biodemo;
